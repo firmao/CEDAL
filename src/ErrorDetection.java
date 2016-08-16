0133780b8edcc3719745472964baed0e7c426315
Andre Valdestilhas <firmao@gmail.com>
2016-08-16 23:12:53 +0200

    Add files via upload

---
+++ src/ErrorDetection.java
@@ -0,0 +1,106 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.hp.hpl.jena.sparql.pfunction.library.str;
+import com.hp.hpl.jena.util.OneToManyMap.Entry;
+
+public class ErrorDetection {
+
+	public static Set<String> lstReport = new HashSet<String>();
+	
+	public static void analyse(Map<String, Set<String>> result) throws IOException {
+		int i = 0;
+		Map<Integer, Set<String>> clusters = new HashMap<Integer, Set<String>>();
+		for (Set<String> value : result.values()) {
+			if (!clusters.containsValue(value)) {
+				clusters.put(i++, value);
+			}
+		}
+		for (Set<String> value : clusters.values()) {
+			getAnalysis(value);
+		}
+		//createReport(lstReport);
+	}
+
+	public static void createReport(Set<String> pLstReport, String dir) throws IOException {
+		PrintWriter writer = new PrintWriter("erroReport_"+dir+".csv", "UTF-8");
+		writer.println("File" + "\t" + "Dataset" + "\t" + "ClusterError");
+		for(String elem : pLstReport)
+		{
+			writer.println(elem);
+		}
+		writer.close();
+		
+//		PrintWriter wFileErr = new PrintWriter("FileErrors_"+dir+".csv", "UTF-8");
+//		writer.println("File" + "\t" + "Error");
+//		for (Map.Entry<String, String> entry : ClosureGenerator.filesProblem.entrySet()) {
+//			wFileErr.println(entry.getKey() + "\t" + entry.getValue());
+//		}
+//		wFileErr.close();
+	}
+
+	private static void getAnalysis(Set<String> cluster) throws IOException {
+		List<String> lstDsCheck = new ArrayList<String>();
+		Map<String, String> clusterDataset = new HashMap<String, String>();
+		for (String elem : cluster) {
+			clusterDataset.put(elem, getDataset(elem));
+		}
+		printErrors(clusterDataset);
+	}
+
+	private static String getDataset(String elem) {
+		String dsName = null;
+		for (Map.Entry<String, Set<String>> entry : ClosureGenerator.datasets.entrySet()) {
+			if (entry.getValue().contains(elem))
+				return entry.getKey();
+		}
+		return dsName;
+	}
+
+	private static void printErrors(Map<String, String> map) throws IOException {
+		Map<String, ArrayList<String>> reverseMap = new HashMap<>(map.entrySet().stream()
+				.collect(Collectors.groupingBy(Map.Entry::getValue)).values().stream()
+				.collect(Collectors.toMap(item -> item.get(0).getValue(),
+						item -> new ArrayList<>(item.stream().map(Map.Entry::getKey).collect(Collectors.toList())))));
+
+		//System.out.println(reverseMap);
+		
+		for (Map.Entry<String, ArrayList<String>> entry : reverseMap.entrySet()) {
+			List<String> lstValues = entry.getValue();
+			String key = entry.getKey();
+			if (lstValues.size() > 1) {
+				String line = key + "\t" + reverseMap.get(key);
+				lstReport.add(line);
+				//System.out.println("An cluster cannot have two or more resources from the same Dataset.");
+				//System.out.println("Dataset: " + key + " has error");
+				//System.out.println("ClusterError: " + reverseMap.get(key));
+			}
+		}
+	}
+
+	public static long getTriples(String fileName) {
+		//String fileName = "sameAs/" + key.split("\t")[0];
+		long lineCount = 0;
+		try {
+		final Path path = Paths.get(fileName);
+			lineCount = Files.lines(path).count();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return lineCount;
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,186 +1,64 @@
 import java.io.File;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
-import com.hp.hpl.jena.vocabulary.OWL;
+import java.util.stream.Collectors;
 
 public class Main {
-
-	static String input = "input2.nt";
-	static String output = "output2.nt";
+	static long triples = 0;
 	public static void main(String[] args) {
-		File dir = new File("c1_to_3");
+		final long start = System.currentTimeMillis();
+		//File dir = new File("sameAs/s8");
+		File dir = new File("sameAs/s8_1");
 		String predicate = "http://www.w3.org/2002/07/owl#sameAs";
-		allMyTCJena(dir, predicate);
-		allMyTCAndre(dir, predicate);
-//		// allMyTC(dir);
-//		// tPellet();
-		allTCPellet(dir);
-		
-		// t1(predicate);
-		// t2();
-		//tRC(predicate);
-		//tSC(predicate);
-		//tTC(predicate);
-		//tClosure(predicate);
+		tClosure(predicate, dir);
+		final long totalTime = System.currentTimeMillis() - start;
+		final String fileName = "summarizarion_" + dir.getName().split("\\\\")[0] + ".txt";
+		Set<String> txtContent = new HashSet<String>();
+		txtContent.add("TotalTime: " + totalTime + "ms");
+		txtContent.add("Triples: " + triples);
+		ClosureGenerator.generateFile(txtContent, fileName);
 	}
 
-	private static void tClosure(String predicate) {
+	private static void tClosure(String predicate, File dir) {
 		try {
-			ClosureAndre.cPredicate = predicate;
-			File f = new File(input);
-			Map<String, Set<String>> result = ClosureAndre.getClosure(f);
-			System.out.println("Closure: " + result);
-			ClosureAndre.generateFile(result, output);
+			String sDir = dir.getName().split("\\\\")[0];
+			ClosureGenerator.cPredicate = predicate;
+			Map<String, Set<String>> result = null;
+			Set<File> setFiles = new HashSet<File>();
+			File[] files = getFiles(dir).stream().toArray(File[]::new);
+			for (int i = 0; i < files.length; i++) {
+				for (int j = i + 1; j < files.length; j++) {
+					setFiles.add(files[i]);
+					setFiles.add(files[j]);
+					try {
+						File f = ClosureGenerator.mixFiles(setFiles);
+						result = ClosureGenerator.getClosure(f);
+						//ClosureGenerator.generateFile(result, output);
+						ErrorDetection.analyse(result);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					setFiles.clear();
+				}
+				triples += ErrorDetection.getTriples(files[i].getAbsolutePath());
+			}
+			ErrorDetection.createReport(ErrorDetection.lstReport, sDir);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static void tTC(String predicate) {
-		try {
-			MyTCJena.cPredicate = predicate;
-			File f = new File(input);
-			Map<String, Set<String>> result = MyTCJena.getTC(f.getAbsolutePath());
-			System.out.println("Transitive Closure: " + result);
-			//MyTCJena.generateFile(result, "output2.nt");
-		} catch (Exception e) {
-			e.printStackTrace();
+	private static Set<File> getFiles(File dir) throws IOException {
+		Set<File> setFiles = null;
+		if (dir.isDirectory()) {
+			setFiles = Files.walk(Paths.get(dir.getPath())).filter(Files::isRegularFile).map(Path::toFile)
+					.collect(Collectors.toSet());
 		}
-	}
-
-	private static void tSC(String predicate) {
-		try {
-			MyTCJena.cPredicate = predicate;
-			File f = new File(input);
-			Map<String, Set<String>> result = MyTCJena.getSC(f.getAbsolutePath());
-			System.out.println("Symmmetric Closure: " + result);
-			//MyTCJena.generateFile(result, "output2.nt");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void tRC(String predicate) {
-		try {
-			MyTCJena.cPredicate = predicate;
-			File f = new File(input);
-			Map<String, Set<String>> result = MyTCJena.getRC(f.getAbsolutePath());
-			System.out.println("Reflexive Closure: " + result);
-			//MyTCJena.generateFile(result, "output2.nt");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void t1(String predicate) {
-		try {
-			MyTCJena.cPredicate = predicate;
-			File f = new File("input2.nt");
-			Map<String, Set<String>> result = MyTCJena.getClosure(f);
-			System.out.println("Output: " + result);
-			MyTCJena.generateFile(result, "output2.nt");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void t2() {
-		try {
-			File f = new File("input.nt");
-			MyTCPellet.closure(OWL.sameAs, f, new File("pellet_output.nt"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void tPellet() {
-		System.out.println("==== owl:sameAs (reflexive + symmetric + transitive) ====");
-		MyTCPellet.closure(OWL.sameAs, new File("input_reflexive_symmetric_transitive.nt"),
-				new File("pellet_output_reflexive_symmetric_transitive.nt"));
-
-		System.out.println("==== saws:hasAncestor (transitive) ====");
-		Property hasAncestor = ResourceFactory.createProperty("http://purl.org/saws/ontology#hasAncestor");
-		MyTCPellet.closure(hasAncestor, new File("input_transitive.nt"), new File("pellet_output_transitive.nt"));
-	}
-
-	public static void allTCPellet(File f) {
-		try {
-			long startTime = System.currentTimeMillis();
-			MyTCPellet.generateAllFilesPellet(f);
-			long endTime = System.currentTimeMillis();
-			long totalTime = endTime - startTime;
-			PrintWriter writer = new PrintWriter("results\\" + f.getName() + "_Pellet.txt", "UTF-8");
-			writer.println("Directory: " + f.getAbsolutePath());
-			writer.println("TotalFiles: " + MyTCPellet.totalFiles);
-			writer.println("TotalTime is: " + totalTime + " ms");
-			writer.println("Number of triples Before TC: " + MyTCPellet.cTriplesBefore);
-			writer.println("Number of triples After TC: " + MyTCPellet.cTriplesAfter);
-			writer.println("Files with problems: " + MyTCPellet.filesProblem.size() +", Problems: " + MyTCPellet.filesProblem.toString());
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void t3() {
-		try {
-			MyTCJena.testReadNtriple("input.nt");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void allMyTC(File f) {
-		try {
-			MyTC.generateAllFiles(f);
-			PrintWriter writer = new PrintWriter("results\\" + f.getName() + ".txt", "UTF-8");
-			writer.println("Directory: " + f.getAbsolutePath());
-			writer.println("TotalFiles: " + MyTC.totalFiles);
-			writer.println("TotalTime is: " + MyTC.totalTime + " ms");
-			writer.println("Number of triples Before TC: " + MyTC.cTriplesBefore);
-			writer.println("Number of triples After TC: " + MyTC.cTriplesAfter);
-			writer.println("Files with problems: " + MyTC.filesProblem.size() +", Problems: " + MyTC.filesProblem.toString());
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void allMyTCJena(File f, String predicate) {
-		try {
-			MyTCJena.generateAllFiles(f, predicate);
-			PrintWriter writer = new PrintWriter("results\\" + f.getName() + "_Jena.txt", "UTF-8");
-			writer.println("Directory: " + f.getAbsolutePath());
-			writer.println("TotalFiles: " + MyTCJena.totalFiles);
-			writer.println("TotalTime is: " + MyTCJena.totalTime + " ms");
-			writer.println("Number of triples Before TC: " + MyTCJena.cTriplesBefore);
-			writer.println("Number of triples After TC: " + MyTCJena.cTriplesAfter);
-			writer.println("Files with problems: " + MyTCJena.filesProblem.size() +", Problems: " + MyTCJena.filesProblem.toString());
-
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void allMyTCAndre(File f, String predicate) {
-		try {
-			ClosureAndre.generateAllFiles(f, predicate);
-			PrintWriter writer = new PrintWriter("results\\" + f.getName() + "_Andre.txt", "UTF-8");
-			writer.println("Directory: " + f.getAbsolutePath());
-			writer.println("TotalFiles: " + ClosureAndre.totalFiles);
-			writer.println("TotalTime is: " + ClosureAndre.totalTime + " ms");
-			writer.println("Number of triples Before TC: " + ClosureAndre.cTriplesBefore);
-			writer.println("Number of triples After TC: " + ClosureAndre.cTriplesAfter);
-			writer.println("Files with problems: " + ClosureAndre.filesProblem.size() +", Problems: " + ClosureAndre.filesProblem.toString());
-
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		return setFiles;
 	}
 }
