7e1714bc180541a33f9ab42e00fa051be80c0e72
Andre Valdestilhas <firmao@gmail.com>
2016-09-18 09:05:32 +0200

    Adjacency List (Graph partition)
    Adjacency List (no need the closures to obtain the clusters)
    Using UnionFind to separate the graph partitions and creating the clusters.

---
+++ src/ErrorDetectionCluster.java
@@ -0,0 +1,91 @@
+package cluster;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ErrorDetectionCluster {
+
+	public static Set<String> lstReport = new HashSet<String>();
+
+	public static void analyse(Map<String, Set<String>> result) throws Exception {
+		int i = 0;
+		Map<Integer, Set<String>> clusters = new HashMap<Integer, Set<String>>();
+		for (Set<String> value : result.values()) {
+			if (!clusters.containsValue(value)) {
+				clusters.put(i++, value);
+			}
+		}
+		for (Set<String> value : clusters.values()) {
+			getAnalysis(value);
+		}
+	}
+
+	public static void createReport(Set<String> pLstReport, String dir) throws IOException {
+		PrintWriter writer = new PrintWriter("erroReport_" + dir + "_parallel.csv", "UTF-8");
+		writer.println("File" + "\t" + "Dataset" + "\t" + "ClusterError");
+		for (String line : pLstReport) {
+			writer.println(line);
+		}
+		writer.close();
+	}
+
+	public static void getAnalysis(Set<String> cluster) throws IOException {
+		Map<String, String> clusterDataset = new HashMap<String, String>();
+		for (String resource : cluster) {
+			clusterDataset.put(resource, getDataset(resource));
+		}
+		printErrors(clusterDataset);
+	}
+
+	private static String getDataset(String resource) {
+		String dsName = null;
+		for (Map.Entry<String, Set<String>> entry : ClosureGeneratorCluster.datasets.entrySet()) {
+			if (entry.getValue().contains(resource))
+				return entry.getKey();
+		}
+		return dsName;
+	}
+
+	private static void printErrors(Map<String, String> map) throws IOException {
+		Map<String, ArrayList<String>> reverseMap = new HashMap<>(map.entrySet().stream()
+				.collect(Collectors.groupingBy(Map.Entry::getValue)).values().stream()
+				.collect(Collectors.toMap(item -> item.get(0).getValue(),
+						item -> new ArrayList<>(item.stream().map(Map.Entry::getKey).collect(Collectors.toList())))));
+
+		// System.out.println(reverseMap);
+
+		for (Map.Entry<String, ArrayList<String>> entry : reverseMap.entrySet()) {
+			List<String> lstValues = entry.getValue();
+			String key = entry.getKey();
+			if (lstValues.size() > 1) {
+				String line = key + "\t" + reverseMap.get(key);
+				lstReport.add(line);
+			}
+		}
+	}
+
+	public static long getTriples(String fileName) {
+		// String fileName = "sameAs/" + key.split("\t")[0];
+		long lineCount = 0;
+		try {
+			final Path path = Paths.get(fileName);
+			lineCount = Files.lines(path).count();
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
+
+		return lineCount;
+	}
+
+}
---
+++ src/MainCluster.java
@@ -0,0 +1,116 @@
+package cluster;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainCluster {
+	static long triples = 0;
+	static Set<String> txtContent = new HashSet<String>();
+	static long timeMixFiles, timeClosures, timeErrorDetection;
+	static long totalSameAsTriples = 19200114;
+
+	public static void main(String[] args) {
+		//File dir = new File("sameAs/sample");
+		//File dir = new File("sameAs/s1000");
+		//File dir = new File("sameAs/s10000");
+		//File dir = new File("sameAs/s100000");
+		//File dir = new File("sameAs/s1000000");
+		File dir = new File("C:\\Users\\andre\\Dropbox\\RunClosuresJar\\sameAs\\s1000");
+		System.out.println("7Available Cores = " + Runtime.getRuntime().availableProcessors());
+		//totalSameAsTriples = countTriples(dir); // TotalSameAsTriples = 19200114
+		System.out.println("TotalSameAsTriples = " + totalSameAsTriples);
+
+		String predicate = "http://www.w3.org/2002/07/owl#sameAs";
+		tClosure(predicate, dir);
+		// final long totalTime = System.currentTimeMillis() - start;
+		final String fileName = "summarizarion_" + dir.getName().split("\\\\")[0] + "_cluster.txt";
+
+		timeClosures = ClosureGeneratorCluster.timeRC + ClosureGeneratorCluster.timeSC
+				+ ClosureGeneratorCluster.timeTC;
+		txtContent.add("TotalTime(MixFiles): " + timeMixFiles + " ms");
+		txtContent.add("TotalTime(Closures): " + timeClosures + " ms");
+		txtContent.add("TotalTime(RC): " + ClosureGeneratorCluster.timeRC + " ms");
+		txtContent.add("TotalTime(SC): " + ClosureGeneratorCluster.timeSC + " ms");
+		txtContent.add("TotalTime(TC): " + ClosureGeneratorCluster.timeTC + " ms");
+		txtContent.add("TotalTime(ErrorDetection): " + timeErrorDetection + " ms");
+		txtContent.add("Available Cores = " + Runtime.getRuntime().availableProcessors());
+
+		final long totalTime = timeMixFiles + timeErrorDetection + timeClosures;
+		txtContent.add("TotalTime: " + totalTime + " ms");
+		txtContent.add("Triples: " + triples);
+		ClosureGeneratorCluster.generateFile(txtContent, fileName);
+	}
+
+	private static Set<File> getFiles(File dir) throws IOException {
+		Set<File> setFiles = null;
+		if (dir.isDirectory()) {
+			setFiles = Files.walk(Paths.get(dir.getPath())).filter(Files::isRegularFile).map(Path::toFile)
+					.collect(Collectors.toSet());
+		}
+		return setFiles;
+	}
+
+	private static long countTriples(File dir) {
+		long ret = 0;
+		try {
+			File[] files = getFiles(dir).stream().toArray(File[]::new);
+			for (int i = 0; i < files.length; i++) {
+				ret += ErrorDetectionCluster.getTriples(files[i].getAbsolutePath());
+			}
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+		return ret;
+	}
+
+	/*
+	 * Adjacency Lists
+	 */
+	private static void tClosure(String predicate, File dir) {
+		try {
+			String sDir = dir.getName().split("\\\\")[0];
+			ClosureGeneratorCluster.cPredicate = predicate;
+			Set<File> setFiles = new HashSet<File>();
+			File[] files = getFiles(dir).stream().toArray(File[]::new);
+			
+			System.out.println("Starting add files...");
+			// IntStream.range(0, files.length).parallel().forEach(i -> {
+			for (int i = 0; i < files.length; i++) {
+				setFiles.add(files[i]);
+				triples += ErrorDetectionCluster.getTriples(files[i].getAbsolutePath());
+			}
+			// });
+			System.out.println("Finished add files.");
+			try {
+				System.out.println("Starting Mix files...");
+				long start = System.currentTimeMillis();
+				Map<String, Set<String>> cluster = ClosureGeneratorCluster.mixFiles(setFiles);
+				timeMixFiles += System.currentTimeMillis() - start;
+				System.out.println("Finished Mix files...");
+
+				System.out.println("Starting clusters and EDA...");
+				// start = System.currentTimeMillis();
+				ClosureGeneratorCluster.analyseCluster(cluster);
+				// timeClosures += System.currentTimeMillis() - start;
+				System.out.println("Finished cluster and EDA...");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			
+			System.out.println("Starting create report...");
+			ErrorDetectionCluster.createReport(ErrorDetectionCluster.lstReport, sDir);
+			System.out.println("Finished create report.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
